9f97aa35c9237ba940a928cc4fe35225cc0b8269
lucasRosssi <lucasrm2305@gmail.com>
2022-03-13 10:17:54 -0300

    created theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+	colors: {
+		background: '#F7F7F7',
+		primary: '#59B7FF',
+		primary_light: '#ADF0FF',
+		secondary: '#454545',
+		shape: '#F5F5F5',
+		success: '#5CD65A',
+		attention: '#E62929',
+		text: '#6E6E6E',
+		title: '#2E2E2E',
+	},
+	fonts: {
+		primary: 'Poppins_400Regular',
+		primary_bold: 'Poppins_700Bold',
+		secondary: 'Rajdhani_400Regular',
+		secondary_bold: 'Rajdhani_700Bold',
+	},
+};
