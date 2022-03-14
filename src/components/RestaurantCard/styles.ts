15b2a0b310e1fa8e5a594f0b3a9d625537b93500
lucasRosssi <lucasrm2305@gmail.com>
2022-03-14 20:44:18 -0300

    fix

--- src/components/RestaurantCard/styles.ts
+++ src/components/RestaurantCard/styles.ts
@@ -1,6 +1,7 @@
-import { BorderlessButton } from 'react-native-gesture-handler';
-import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import { BorderlessButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
 	width: 100%;
