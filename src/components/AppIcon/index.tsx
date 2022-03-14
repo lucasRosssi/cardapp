e49e97852f886616e1c2497a6479a61757507fd5
lucasRosssi <lucasrm2305@gmail.com>
2022-03-14 00:51:01 -0300

    added new icon

--- src/components/AppIcon/index.tsx
+++ src/components/AppIcon/index.tsx
@@ -1,37 +1,31 @@
 import React from 'react';
 
-import { Feather, Ionicons } from '@expo/vector-icons';
+import { Feather, Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 interface AppIconProps {
-	name: 'menu' | 'settings' | 'location';
+	name: 'menu' | 'settings' | 'location' | 'food-menu' | 'gallery';
 	size?: number;
 	color?: string;
 }
 
-export function AppIcon({ name, size, color }: AppIconProps) {
+export function AppIcon({ name, size = RFValue(30), color }: AppIconProps) {
 	return (
 		<>
 			{name === 'menu' && (
-				<Feather
-					name="menu"
-					size={size ? RFValue(size) : RFValue(30)}
-					color={color}
-				/>
+				<Feather name="menu" size={RFValue(size)} color={color} />
 			)}
 			{name === 'settings' && (
-				<Ionicons
-					name="settings-sharp"
-					size={size ? RFValue(size) : RFValue(30)}
-					color={color}
-				/>
+				<Ionicons name="settings-sharp" size={RFValue(size)} color={color} />
 			)}
 			{name === 'location' && (
-				<Ionicons
-					name="location-sharp"
-					size={size ? RFValue(size) : RFValue(30)}
-					color={color}
-				/>
+				<Ionicons name="location-sharp" size={RFValue(size)} color={color} />
+			)}
+			{name === 'food-menu' && (
+				<MaterialIcons name="menu-book" size={RFValue(size)} color={color} />
+			)}
+			{name === 'gallery' && (
+				<Ionicons name="camera" size={RFValue(size)} color={color} />
 			)}
 		</>
 	);
