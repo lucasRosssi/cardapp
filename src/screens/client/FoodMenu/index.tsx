645d01498e354dd0a07f4eb55447bf886c1d3f6a
lucasRosssi <lucasrm2305@gmail.com>
2022-03-14 00:52:43 -0300

    created FoodMenu screen

---
+++ src/screens/client/FoodMenu/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Header } from '../../../components/Header';
+
+import { Container } from './styles';
+
+export function FoodMenu() {
+	return (
+		<Container>
+			<Header />
+		</Container>
+	);
+}
