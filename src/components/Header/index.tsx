e1958afb28de2f94ab1085bc6edeafaf4ad59d3c
lucasRosssi <lucasrm2305@gmail.com>
2022-03-14 00:51:22 -0300

    worked on Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import { useTheme } from 'styled-components';
 
-import { AppIcon } from '../AppIcon';
-
-import { Container, Button, Logo } from './styles';
+import { Container, User, Picture, Username, Logo } from './styles';
 
 export function Header() {
 	const theme = useTheme();
 
 	return (
 		<Container>
-			<Button>
-				<AppIcon name="menu" color={theme.colors.shape} />
-			</Button>
+			<User>
+				<Picture source={{ uri: 'https://www.github.com/lucasRosssi.png' }} />
+				<Username>Lucas</Username>
+			</User>
 
 			<Logo>Cardapp</Logo>
-
-			<Button>
-				<AppIcon name="settings" color={theme.colors.shape} />
-			</Button>
 		</Container>
 	);
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components/native';
-import { BorderlessButton } from 'react-native-gesture-handler';
+
 import { RFValue } from 'react-native-responsive-fontsize';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
@@ -10,15 +10,35 @@ export const Container = styled.View`
 
 	flex-direction: row;
 	justify-content: space-between;
-	align-items: flex-end;
+	align-items: center;
 
-	padding: ${getStatusBarHeight() + 20}px 20px 20px;
+	padding: ${getStatusBarHeight() + 20}px 30px 20px 20px;
+
+	elevation: 10;
 `;
-
-export const Button = styled(BorderlessButton)``;
 
 export const Logo = styled.Text`
 	font-family: ${({ theme }) => theme.fonts.secondary_bold};
-	font-size: ${RFValue(25)}px;
+	font-size: ${RFValue(30)}px;
+	color: ${({ theme }) => theme.colors.shape};
+`;
+
+export const User = styled.View`
+	flex-direction: row;
+	align-items: center;
+`;
+
+export const Picture = styled.Image`
+	width: ${RFValue(50)}px;
+	height: ${RFValue(50)}px;
+
+	border-radius: ${RFValue(25)}px;
+
+	margin-right: 10px;
+`;
+
+export const Username = styled.Text`
+	font-family: ${({ theme }) => theme.fonts.secondary_bold};
+	font-size: ${RFValue(20)}px;
 	color: ${({ theme }) => theme.colors.shape};
 `;
