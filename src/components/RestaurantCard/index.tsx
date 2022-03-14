e6d8cba02d2c7c5740f2d30eea4a265fbf4d61f9
lucasRosssi <lucasrm2305@gmail.com>
2022-03-14 00:51:45 -0300

    worked on RestaurantCard

--- src/components/RestaurantCard/index.tsx
+++ src/components/RestaurantCard/index.tsx
@@ -3,24 +3,53 @@ import { useTheme } from 'styled-components';
 import { AppIcon } from '../AppIcon';
 import { Separator } from '../Separator';
 
-import { Container, Header, Name, LocationButton, Content } from './styles';
+import {
+	Container,
+	Header,
+	Name,
+	Button,
+	Content,
+	Picture,
+	LeftContent,
+	RightContent,
+} from './styles';
 
 export function RestaurantCard() {
 	const theme = useTheme();
 
 	return (
 		<Container>
-			<Header>
-				<Name>Wonderful</Name>
+			<LeftContent>
+				<Header>
+					<Name>Restaurante Tudo de Bom</Name>
+				</Header>
 
-				<LocationButton>
-					<AppIcon name="location" color={theme.colors.shape} size={23} />
-				</LocationButton>
-			</Header>
+				<Separator color={theme.colors.separator} />
 
-			<Separator color={theme.colors.shape} />
+				<Content>
+					<Picture
+						source={{
+							uri: 'https://vinicolaaraucaria.com.br/wp-content/uploads/2020/06/interna_restaurante-1-1400x700.jpg',
+						}}
+					/>
+				</Content>
+			</LeftContent>
 
-			<Content></Content>
+			<Separator vertical color={theme.colors.separator} />
+
+			<RightContent>
+				<Button>
+					<AppIcon name="location" color={theme.colors.primary} size={25} />
+				</Button>
+
+				<Button>
+					<AppIcon name="food-menu" color={theme.colors.primary} size={25} />
+				</Button>
+
+				<Button>
+					<AppIcon name="gallery" color={theme.colors.primary} size={25} />
+				</Button>
+			</RightContent>
 		</Container>
 	);
 }
--- src/components/RestaurantCard/styles.ts
+++ src/components/RestaurantCard/styles.ts
@@ -4,26 +4,57 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
 	width: 100%;
-	background-color: ${({ theme }) => theme.colors.primary};
+	background-color: ${({ theme }) => theme.colors.shape};
 
 	border-radius: 20px;
 
-	padding: 12px;
+	flex-direction: row;
+
+	padding: 12px 0;
+	margin-bottom: 25px;
+
+	elevation: 10;
+`;
+
+export const LeftContent = styled.View`
+	width: 80%;
+
+	padding: 0 12px;
 `;
 
 export const Header = styled.View`
 	flex-direction: row;
 	justify-content: space-between;
+	align-items: center;
 
-	margin-bottom: 3px;
+	margin-bottom: 10px;
 `;
 
 export const Name = styled.Text`
 	font-family: ${({ theme }) => theme.fonts.primary_bold};
 	font-size: ${RFValue(14)}px;
-	color: ${({ theme }) => theme.colors.shape};
+	color: ${({ theme }) => theme.colors.primary};
 `;
 
-export const LocationButton = styled(BorderlessButton)``;
+export const Content = styled.View`
+	margin-top: 15px;
+`;
 
-export const Content = styled.View``;
+export const Picture = styled.Image`
+	width: 100%;
+	height: ${RFValue(150)}px;
+
+	border-radius: 10px;
+`;
+
+export const RightContent = styled.View`
+	width: 20%;
+
+	align-items: center;
+
+	padding: 0 12px;
+`;
+
+export const Button = styled(BorderlessButton)`
+	margin-bottom: 12px;
+`;
