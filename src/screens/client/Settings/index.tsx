48424508456b9bb47ebcc7cf721a42ec6fc4b34e
lucasRosssi <lucasrm2305@gmail.com>
2022-03-24 17:06:39 -0300

    worked on Settings screen

--- src/screens/client/Settings/index.tsx
+++ src/screens/client/Settings/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Header } from '../../../components/Header';
+import { SettingsItem } from '../../../components/SettingsItem';
+import { settingsItems } from '../../../data/lists';
+
+import { Container, SettingsList } from './styles';
 
 export function Settings() {
-	return <Container></Container>;
+	const item_list = settingsItems.map(({ title, icon }) => (
+		<SettingsItem key={title} title={title} icon={icon} />
+	));
+
+	return (
+		<Container>
+			<Header />
+
+			<SettingsList>{item_list}</SettingsList>
+		</Container>
+	);
 }
