f7ffddaec09abea12be34479825fd03edbf50426
lucasRosssi <lucasrm2305@gmail.com>
2022-04-03 20:45:22 -0300

    created CompanyDashboard screen

--- src/screens/company/CompanyDashboard/index.tsx
+++ src/screens/company/CompanyDashboard/index.tsx
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { useTheme } from 'styled-components';
+import { AppIcon } from '../../../components/AppIcon';
+import { Category } from '../../../components/Category';
 import { Header } from '../../../components/Header';
+import { MenuDTO } from '../../../dtos/EstablishmentDTO';
+import { useAuth } from '../../../hooks/useAuth';
+import { api } from '../../../services/api';
 
-import { Container } from './styles';
+import { Container, CategoriesList, AddCategoryButton, Title } from './styles';
 
 export function CompanyDashboard() {
+	const theme = useTheme();
+	const { company } = useAuth();
+
+	const [isLoading, setIsLoading] = useState(true);
+	const [categories, setCategories] = useState<MenuDTO[]>([]);
+
+	async function fetchCategories() {
+		const response = await api.get(`/establishments/${company.id}`);
+		setCategories(response.data.menu);
+		setIsLoading(false);
+	}
+
+	useEffect(() => {
+		fetchCategories();
+	}, []);
+
 	return (
 		<Container>
 			<Header />
+
+			{isLoading ? (
+				<>
+					<Title style={{ padding: 20 }}>Categorias</Title>
+					<ActivityIndicator color={theme.colors.primary} size={50} />
+				</>
+			) : (
+				<CategoriesList
+					ListHeaderComponent={<Title>Categorias</Title>}
+					data={categories}
+					keyExtractor={(item) => item.id}
+					renderItem={({ item }) => (
+						<Category
+							id={item.id}
+							category={item.category}
+							dishes={item.dishes}
+						/>
+					)}
+				/>
+			)}
+
+			<AddCategoryButton>
+				<AppIcon name="plus" color={theme.colors.primary} />
+			</AddCategoryButton>
 		</Container>
 	);
 }
--- src/screens/company/CompanyDashboard/styles.ts
+++ src/screens/company/CompanyDashboard/styles.ts
@@ -1,5 +1,41 @@
+import { FlatList, FlatListProps } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
+import { MenuDTO } from '../../../dtos/EstablishmentDTO';
 
 export const Container = styled.View`
 	flex: 1;
+	background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const Title = styled.Text`
+	font-family: ${({ theme }) => theme.fonts.primary_bold};
+	font-size: ${RFValue(16)}px;
+	color: ${({ theme }) => theme.colors.title};
+
+	margin-bottom: 20px;
+`;
+
+export const CategoriesList = styled(
+	FlatList as new (props: FlatListProps<MenuDTO>) => FlatList<MenuDTO>
+).attrs({
+	padding: 20,
+})``;
+
+export const AddCategoryButton = styled(RectButton)`
+	position: absolute;
+	bottom: 20px;
+	align-self: center;
+
+	width: ${RFValue(60)}px;
+	height: ${RFValue(60)}px;
+	background-color: ${({ theme }) => theme.colors.full_light};
+
+	border-radius: ${RFValue(30)}px;
+
+	align-items: center;
+	justify-content: center;
+
+	elevation: 6;
 `;
