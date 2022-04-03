e58c443e97c03f691d9e23676ab136691e7e0617
lucasRosssi <lucasrm2305@gmail.com>
2022-04-03 19:37:03 -0300

    added company params lists

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,9 +1,13 @@
-import { RootTabParamList } from '../routes/client/tab.routes';
-import { RootStackParamList } from '../routes/client/stack.routes';
+import { ClientTabParamList } from '../routes/client/tab.routes';
+import { ClientStackParamList } from '../routes/client/stack.routes';
+import { CompanyTabParamList } from '../routes/company/tab.routes';
+import { CompanyStackParamList } from '../routes/company/stack.routes';
 
 declare global {
 	namespace ReactNavigation {
-		interface RootParamList extends RootTabParamList {}
-		interface RootParamList extends RootStackParamList {}
+		interface RootParamList extends ClientTabParamList {}
+		interface RootParamList extends CompanyTabParamList {}
+		interface RootParamList extends ClientStackParamList {}
+		interface RootParamList extends CompanyStackParamList {}
 	}
 }
