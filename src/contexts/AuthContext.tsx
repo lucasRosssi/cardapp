eea1f56c6fefebac314a59235a969eca97a5df21
lucasRosssi <lucasrm2305@gmail.com>
2022-04-03 19:37:46 -0300

    created company context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,12 @@
 import React, { createContext, ReactNode, useState } from 'react';
+import { EstablishmentDTO } from '../dtos/EstablishmentDTO';
 import { UserDTO } from '../dtos/UserDTO';
 
 export interface IAuthContextData {
 	user: UserDTO;
+	company: EstablishmentDTO;
 	handleUpdateUser: (data: UserDTO) => void;
+	handleUpdateCompany: (data: EstablishmentDTO) => void;
 }
 
 interface AuthContextProps {
@@ -19,16 +22,34 @@ export function AuthProvider({ children }: AuthContextProps) {
 		city: 'Niterói',
 		picture: 'https://www.github.com/lucasRosssi.png',
 	} as UserDTO);
+	const [company, setCompany] = useState<EstablishmentDTO>({
+		id: '2',
+		name: 'The Cruise Bar',
+		address: '',
+		picture:
+			'https://galeriemagazine.com/wp-content/uploads/2020/01/MAIN_BG_Goodman_Environmental_Hero_0007_RT-1920x1200.jpg',
+		contact: {
+			email: 'thecruisebar@bar.com',
+			phone: '2124246969',
+		},
+		menu: [],
+	} as EstablishmentDTO);
 
 	function handleUpdateUser(data: UserDTO) {
 		setUser(data);
+	}
+
+	function handleUpdateCompany(data: EstablishmentDTO) {
+		setCompany(data);
 	}
 
 	return (
 		<AuthContext.Provider
 			value={{
 				user,
+				company,
 				handleUpdateUser,
+				handleUpdateCompany,
 			}}
 		>
 			{children}
