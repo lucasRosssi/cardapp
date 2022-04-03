274978706ea7d7573f2d3c5da3ced42c6775c0ab
lucasRosssi <lucasrm2305@gmail.com>
2022-04-03 19:38:14 -0300

    changed initial route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { ClientStackRoutes } from './client/stack.routes';
+import { CompanyStackRoutes } from './company/stack.routes';
 
 export function Routes() {
 	return (
 		<NavigationContainer>
-			<ClientStackRoutes />
+			<CompanyStackRoutes />
 		</NavigationContainer>
 	);
 }
