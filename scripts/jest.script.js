182f25b3acd34238ff4a4265db514c6141f7fffd
lucasRosssi <lucasrm2305@gmail.com>
2022-04-09 15:43:01 -0300

    create jest script file

---
+++ scripts/jest.script.js
@@ -0,0 +1,6 @@
+console.clear();
+console.log('');
+console.log(' ------------------------------------------------------------');
+console.log('');
+console.log('\x1b[44m%s\x1b[0m', ' STARTING TEST... ');
+console.log('');
