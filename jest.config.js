1dc01bc6f05dbc0addadcd367e41bab3361faf08
lucasRosssi <lucasrm2305@gmail.com>
2022-04-09 15:42:25 -0300

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
 module.exports = {
 	preset: 'jest-expo',
-	testPathIgnorePatterns: ['/node_modules', '/android', '/ios', '/.expo'],
+	testPathIgnorePatterns: [
+		'/node_modules',
+		'/android',
+		'/ios',
+		'/.expo',
+		'/scripts',
+	],
 	setupFilesAfterEnv: [
 		'@testing-library/jest-native/extend-expect',
 		'jest-styled-components',
+		'./node_modules/react-native-gesture-handler/jestSetup.js',
 	],
 };
