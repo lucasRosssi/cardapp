719a3c3e1c37efec26ea963a3995b30e684995e3
lucasRosssi <lucasrm2305@gmail.com>
2022-04-13 21:05:00 -0300

    applied moti

--- src/components/DishCard/index.tsx
+++ src/components/DishCard/index.tsx
@@ -7,6 +7,7 @@ import { Separator } from '../Separator';
 import { Container, Content, Likes, Name, Picture, Price } from './styles';
 import { useAuth } from '../../hooks/useAuth';
 import { AppIcon } from '../AppIcon';
+import { MotiView } from 'moti';
 
 interface DishCardProps {
 	name: string;
@@ -49,25 +50,31 @@ export function DishCard({
 	}
 
 	return (
-		<Container testID="card-dish" onPress={readDetails}>
-			<Picture
-				source={{
-					uri: picture,
-				}}
-			/>
+		<MotiView
+			from={{ translateX: 30, opacity: 0 }}
+			animate={{ translateX: 0, opacity: 1 }}
+			transition={{ type: 'timing', duration: 500 }}
+		>
+			<Container testID="card-dish" onPress={readDetails}>
+				<Picture
+					source={{
+						uri: picture,
+					}}
+				/>
 
-			<Separator vertical color={theme.colors.separator} />
+				<Separator vertical color={theme.colors.separator} />
 
-			<Content>
-				<Name>{name}</Name>
+				<Content>
+					<Name>{name}</Name>
 
-				<Price>R$ 20,00</Price>
+					<Price>R$ {price}</Price>
 
-				<Likes>
-					<AppIcon name="like-fill" color={theme.colors.primary} size={10} />{' '}
-					{like_count}
-				</Likes>
-			</Content>
-		</Container>
+					<Likes>
+						<AppIcon name="like-fill" color={theme.colors.primary} size={10} />{' '}
+						{like_count}
+					</Likes>
+				</Content>
+			</Container>
+		</MotiView>
 	);
 }
