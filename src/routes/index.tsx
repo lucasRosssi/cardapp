80b9e8e9c56b5fec1d53faddddbf16e72468bc16
lucasRosssi <lucasrm2305@gmail.com>
2022-05-06 20:28:26 -0300

    new route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { ClientStackRoutes } from './client/stack.routes';
 import { CompanyStackRoutes } from './company/stack.routes';
+import { AuthRoutes } from './auth/auth.routes';
 
 export function Routes() {
 	return (
 		<NavigationContainer>
-			<ClientStackRoutes />
+			<AuthRoutes />
 		</NavigationContainer>
 	);
 }
