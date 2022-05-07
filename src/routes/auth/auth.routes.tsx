20c5d614f1c45f5b9403ea3f6c18f0e6e04b1d97
lucasRosssi <lucasrm2305@gmail.com>
2022-05-06 21:29:56 -0300

    added new screen to navigator

--- src/routes/auth/auth.routes.tsx
+++ src/routes/auth/auth.routes.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { Login } from '../../screens/auth/Login';
+import { Register } from '../../screens/auth/Register';
 
 export type AuthParamList = {
 	Login: undefined;
+	Register: undefined;
 };
 
 const { Navigator, Screen } = createStackNavigator<AuthParamList>();
@@ -13,6 +15,7 @@ export function AuthRoutes() {
 	return (
 		<Navigator screenOptions={{ headerShown: false }}>
 			<Screen name="Login" component={Login} />
+			<Screen name="Register" component={Register} />
 		</Navigator>
 	);
 }
