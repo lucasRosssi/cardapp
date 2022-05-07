2cdca9396328a06951fd02cfef81b69b4a76da14
lucasRosssi <lucasrm2305@gmail.com>
2022-05-06 21:28:11 -0300

    added auth route types

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,3 +1,4 @@
+import { AuthParamList } from '../routes/auth/auth.routes';
 import { ClientTabParamList } from '../routes/client/tab.routes';
 import { ClientStackParamList } from '../routes/client/stack.routes';
 import { CompanyTabParamList } from '../routes/company/tab.routes';
@@ -5,6 +6,7 @@ import { CompanyStackParamList } from '../routes/company/stack.routes';
 
 declare global {
 	namespace ReactNavigation {
+		interface RootParamList extends AuthParamList {}
 		interface RootParamList extends ClientTabParamList {}
 		interface RootParamList extends CompanyTabParamList {}
 		interface RootParamList extends ClientStackParamList {}
