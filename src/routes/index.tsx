d85d1d3c2274c2b30c2a1442bde60a739819e102
lucasRosssi <lucasrm2305@gmail.com>
2022-05-06 22:22:51 -0300

    added switch case to render route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useAuth } from '../hooks/useAuth';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { ClientStackRoutes } from './client/stack.routes';
@@ -6,9 +7,18 @@ import { CompanyStackRoutes } from './company/stack.routes';
 import { AuthRoutes } from './auth/auth.routes';
 
 export function Routes() {
-	return (
-		<NavigationContainer>
-			<AuthRoutes />
-		</NavigationContainer>
-	);
+	const { authStatus } = useAuth();
+
+	function getRoute() {
+		switch (authStatus) {
+			case null:
+				return <AuthRoutes />;
+			case 'user':
+				return <ClientStackRoutes />;
+			case 'company':
+				return <CompanyStackRoutes />;
+		}
+	}
+
+	return <NavigationContainer>{getRoute()}</NavigationContainer>;
 }
