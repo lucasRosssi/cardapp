b065645c9468c5b2377e74ffd5185f2c335f3a91
lucasRosssi <lucasrm2305@gmail.com>
2022-06-18 00:25:12 -0300

    feat: render 6 per batch

--- src/screens/client/ClientDashboard/index.tsx
+++ src/screens/client/ClientDashboard/index.tsx
@@ -1,5 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { Alert } from 'react-native';
+
 import { api } from '../../../services/api';
+import axios from 'axios';
 
 import { Header } from '../../../components/Header';
 import { EstablishmentCard } from '../../../components/EstablishmentCard';
@@ -7,30 +10,57 @@ import { LoadingIndicator } from '../../../components/LoadingIndicator';
 
 import { EstablishmentDTO } from '../../../dtos/EstablishmentDTO';
 
-import { Container, Content, EstablishmentsList, Title } from './styles';
+import { Container, EstablishmentsList, Title } from './styles';
 
 export function ClientDashboard() {
 	const [isMounted, setIsMounted] = useState(true);
 	const [isLoading, setIsLoading] = useState(true);
 	const [establishments, setEstablishments] = useState<EstablishmentDTO[]>([]);
+	const [slicedEstablishments, setSlicedEstablishments] = useState<
+		EstablishmentDTO[]
+	>([]);
+	const [sliceStartIndex, setSliceStartIndex] = useState(6);
+	const [sliceEndIndex, setSliceEndIndex] = useState(12);
 
-	async function fetchEstablishments() {
+	const fetchEstablishments = useCallback(async () => {
 		try {
-			const response = await api.get('/establishments');
-			setEstablishments(response.data);
+			const { data } = await api.get<EstablishmentDTO[]>('/establishments');
+
+			setEstablishments(data);
+			setSlicedEstablishments(data.slice(0, 6));
 		} catch (error) {
-			throw new Error('### Oh no... ###');
+			if (axios.isAxiosError(error)) {
+				Alert.alert('', error.message);
+			} else {
+				Alert.alert('Erro', 'Falha ao carregar os dados dos estabelecimentos');
+			}
 		} finally {
 			setIsLoading(false);
 		}
-	}
+	}, []);
+
+	const getNextEstablishments = useCallback(() => {
+		const nextEstablishments = establishments.slice(
+			sliceStartIndex,
+			sliceEndIndex
+		);
+
+		setSlicedEstablishments((oldState) => [...oldState, ...nextEstablishments]);
+		setSliceStartIndex((oldState) => oldState + 6);
+		setSliceEndIndex((oldState) => oldState + 6);
+	}, []);
+
+	const isThereMoreEstablishments = useMemo(
+		() => slicedEstablishments.length < establishments.length,
+		[establishments, slicedEstablishments]
+	);
 
 	useEffect(() => {
 		if (isMounted) {
 			fetchEstablishments();
 		}
 
-		console.log(establishments);
+		console.log(isThereMoreEstablishments);
 
 		return () => setIsMounted(false);
 	}, []);
@@ -39,25 +69,29 @@ export function ClientDashboard() {
 		<Container>
 			<Header />
 
-			<Content>
-				{isLoading ? (
-					<LoadingIndicator />
-				) : (
-					<EstablishmentsList
-						testID="establishment-card"
-						data={establishments}
-						keyExtractor={(item) => String(item.id)}
-						renderItem={({ item }) => (
-							<EstablishmentCard
-								name={item.name}
-								picture={item.picture}
-								menu={item.menus}
-							/>
-						)}
-						ListHeaderComponent={<Title>Estabelecimentos</Title>}
-					/>
-				)}
-			</Content>
+			{isLoading ? (
+				<LoadingIndicator />
+			) : (
+				<EstablishmentsList
+					testID="establishment-card"
+					data={slicedEstablishments}
+					keyExtractor={(item) => String(item.id)}
+					renderItem={({ item }) => (
+						<EstablishmentCard
+							name={item.name}
+							picture={item.picture}
+							menu={item.menus}
+						/>
+					)}
+					ListHeaderComponent={<Title>Estabelecimentos</Title>}
+					ListFooterComponent={
+						isThereMoreEstablishments ? <LoadingIndicator /> : null
+					}
+					onEndReached={
+						isThereMoreEstablishments ? getNextEstablishments : undefined
+					}
+				/>
+			)}
 		</Container>
 	);
 }
