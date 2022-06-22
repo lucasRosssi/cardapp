9ae5fda08d74fa744691eade3b0e6ee98feee86a
lucasRosssi <lucasrm2305@gmail.com>
2022-06-21 22:11:18 -0300

    refactor: rework LoadingIndicator

--- src/components/LoadingIndicator/index.tsx
+++ src/components/LoadingIndicator/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { ActivityIndicator } from 'react-native';
-import { useTheme } from 'styled-components/native';
 
-import { Container } from './styles';
+import { Container, Loading } from './styles';
 
-export function LoadingIndicator() {
-	const theme = useTheme();
-
+export const LoadingIndicator: React.FC = () => {
 	return (
 		<Container testID="loading">
-			<ActivityIndicator color={theme.colors.primary} size={50} />
+			<Loading />
 		</Container>
 	);
-}
+};
--- src/components/LoadingIndicator/styles.ts
+++ src/components/LoadingIndicator/styles.ts
@@ -6,3 +6,8 @@ export const Container = styled.View`
 	align-items: center;
 	justify-content: center;
 `;
+
+export const Loading = styled.ActivityIndicator.attrs(({ theme }) => ({
+	color: theme.colors.primary,
+	size: 50,
+}))``;
